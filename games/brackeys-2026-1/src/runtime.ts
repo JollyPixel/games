a4033562b7b183614d74e9a0f526f8775943fbc8
GENTILHOMME Thomas <gentilhomme.thomas@gmail.com>
2026-02-15 21:35:52 +0100

    chore: add ambiant sound

--- games/brackeys-2026-1/src/runtime.ts
+++ games/brackeys-2026-1/src/runtime.ts
@@ -1,23 +1,52 @@
 // Import Third-party Dependencies
 import { Player, loadPlayer } from "@jolly-pixel/runtime";
+import {
+  GlobalAudioManager,
+  AudioBackground
+} from "@jolly-pixel/engine";
 
 // Import Internal Dependencies
 import { createDefaultScene } from "./scenes/default.ts";
 
-const canvas = document.querySelector("canvas");
-if (!canvas) {
+const canvasHTMLElement = document.querySelector("canvas");
+if (!canvasHTMLElement) {
   throw new Error("HTMLCanvasElement not found");
 }
 
 const debug = true;
 
-const player = new Player(canvas, {
+const player = new Player(canvasHTMLElement, {
   // Displays a stats.js FPS panel — useful during development
   includePerformanceStats: debug
 });
 
+const world = player.gameInstance;
+
 createDefaultScene(player.gameInstance, {
   debug
+});
+
+const audioManager = GlobalAudioManager.fromGameInstance(world);
+const bg = new AudioBackground({
+  audioManager,
+  autoPlay: false,
+  playlists: [{
+    name: "main",
+    onEnd: "loop",
+    tracks: [
+      {
+        name: "ambiant",
+        path: "sounds/ambiant.ogg"
+      }
+    ]
+  }]
+});
+
+world.audio.observe(bg);
+world.audio.volume = 0.25;
+
+canvasHTMLElement.addEventListener("click", async() => {
+  await bg.play("main.ambiant");
 });
 
 loadPlayer(player)
